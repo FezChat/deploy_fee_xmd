a80f5e28ce5c716c6fb2cac2ede5153840589978
FeeSpace <frediezra001@gmail.com>
2025-12-24 05:33:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
+// Import required modules
 const express = require('express'); // Import the Express framework
 const bodyParser = require("body-parser"); // Import body-parser
 const path = require('path'); // Import path module for handling file paths
 
-const app = express(); // Initialize the Express application
-const PORT = process.env.PORT || 8000; // Set the port
+// Initialize the Express application
+const app = express(); 
+const PORT = process.env.PORT || 8000; // Set the port to either the environment variable PORT or 8000
 
 // Use body-parser middleware to parse JSON and URL-encoded bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define a route for the root URL
-app.use('/', async (req, res, next) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html')); // Send the index.html file
 });
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Fr El Ez 🚨 Server running on http://localhost:${PORT}`); // Log the server URL
+    console.log(`🚨 Server running on http://localhost:${PORT}`); // Log the server URL
 });
 
 // Export the app for testing or further usage
