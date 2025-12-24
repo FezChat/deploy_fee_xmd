2b3184e42ece5b352477d07f0db743aff3fa1669
FeeSpace <frediezra001@gmail.com>
2025-12-24 04:34:49 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,21 @@
+{
+  "name": "fee-xmd-dashboard",
+  "version": "1.0.0",
+  "description": "FEE XMD Deployment Dashboard",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5",
+    "dotenv": "^16.3.1"
+  },
+  "engines": {
+    "node": ">=16.0.0"
+  },
+  "keywords": ["whatsapp", "bot", "dashboard", "deployment"],
+  "author": "Fredi AI Tech",
+  "license": "MIT"
+}
